8e0ba9bcb2751bf328fe18e965b6c2d38c1fbcda
Davi Bernardes Machado <davimachado91210@gmail.com>
2025-04-08 19:04:56 -0300

    Revert "v2"

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    MYSQL_DB = os.getenv('MYSQL_DB', 'db_fastapi_p1')
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '481516$')
-    MYSQL_PORT = os.getenv('MYSQL_PORT', '3307')
