d79f10969795c5d72d37795621134a48239377d2
Matheus Salinas Zancopé <146689163+Matheus-Salinas@users.noreply.github.com>
2025-04-08 23:47:52 -0300

    Revert "V2"

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    MYSQL_DB = os.getenv('MYSQL_DB', 'gerenciamento_produtos')
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'manager')
-    MYSQL_PORT = os.getenv('MYSQL_PORT', '3306')
